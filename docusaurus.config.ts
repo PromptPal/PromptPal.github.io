f5de6365ea2dde661e610232fe8e694a7170306d
AnnatarHe <annatar.he+github@gmail.com>
2024-02-24 16:54:40 +0800

    feat(blog): add release hero component

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
           //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: ["./src/css/custom.css", "./src/css/raycast.css"],
         },
       } satisfies Preset.Options,
     ],
---
+++ src/components/ReleaseHero/index.tsx
@@ -0,0 +1,36 @@
+import styles from "./styles.module.css";
+import clsx from "clsx";
+import logo from "@site/static/img/logo.png";
+console.log("logo", logo);
+
+type RelaseHeroProps = {
+  version: string;
+  features: string[];
+};
+function ReleaseHero(props: RelaseHeroProps) {
+  const { version, features } = props;
+  return (
+    <div className={styles.container}>
+      <img src={logo} alt="promptpal logo" className={styles.logo} />
+      <div>
+        <h2 className={styles.version}>Release {version}</h2>
+        <ul className={styles.features}>
+          {features.map((x) => (
+            <li
+              key={x}
+              className={clsx(
+                "raycast-tag",
+                styles.feature,
+                styles.featureText,
+              )}
+            >
+              {x}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ReleaseHero;
