b9d16a95c7f5ecb0ab05ac8d7718eb9fe92cf6d2
AnnatarHe <annatar.he+github@gmail.com>
2024-03-31 23:08:36 +0800

    feat(docs): update docs

--- src/components/ReleaseHero/index.tsx
+++ src/components/ReleaseHero/index.tsx
@@ -1,7 +1,6 @@
 import styles from "./styles.module.css";
 import clsx from "clsx";
 import logo from "@site/static/img/logo.png";
-console.log("logo", logo);
 
 type RelaseHeroProps = {
   version: string;
